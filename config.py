8737e6de10b43faac77dbba253b23a48f327232a
kyskskgdmgxbzbsg <linuxofficial41@gmail.com>
2025-05-24 16:42:46 +0530

    Update config.py

--- config.py
+++ config.py
@@ -4,11 +4,11 @@ from dotenv import load_dotenv
 # Load .env file content into environment variables
 load_dotenv()
 
-API_ID = int(os.getenv("API_ID", ""))
-API_HASH = os.getenv("API_HASH", "")
-BOT_TOKEN = os.getenv("BOT_TOKEN", "")
-MONGO_DB_URI = os.getenv("MONGO_DB_URI", "")
-OWNER_ID = int(os.getenv("OWNER_ID", ""))
-LOG_GROUP_ID = int(os.getenv("LOG_GROUP_ID", ""))
+API_ID = int(os.getenv("API_ID", "21546320"))
+API_HASH = os.getenv("API_HASH", "c16805d6f2393d35e7c49527daa317c7")
+BOT_TOKEN = os.getenv("BOT_TOKEN", "8020578503:AAFWeiecAUXOmzoOIzzTvnZ8BdcluskMSVk")
+MONGO_DB_URI = os.getenv("MONGO_DB_URI", "mongodb+srv://manoranjanhor43:somuxd@manoranjan.wsglmdq.mongodb.net/?retryWrites=true&w=majority&appName=Manoranjan")
+OWNER_ID = int(os.getenv("OWNER_ID", "6908972904"))
+LOG_GROUP_ID = int(os.getenv("LOG_GROUP_ID", "-1002100433415"))
 
 # You can add more config variables here if needed
