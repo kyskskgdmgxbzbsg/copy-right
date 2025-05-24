c69d505486e4773615cbac0bfdc7938fb2da4066
kyskskgdmgxbzbsg <linuxofficial41@gmail.com>
2025-05-24 15:55:56 +0530

    Delete bot/handler directory

--- bot/handler/callbacks.py
+++
@@ -1,9 +0,0 @@
-# bot/handlers/callbacks.py
-
-from pyrogram import Client, filters
-from pyrogram.types import CallbackQuery
-
-@Client.on_callback_query(filters.regex("close"))
-async def close_message(client: Client, callback_query: CallbackQuery):
-    await callback_query.message.delete()
-    await callback_query.answer()
