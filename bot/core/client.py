94fc020b0865d215b71b50ea3cda91ce1cfb70b8
kyskskgdmgxbzbsg <linuxofficial41@gmail.com>
2025-05-24 17:42:45 +0530

    Update client.py

--- bot/core/client.py
+++ bot/core/client.py
@@ -2,7 +2,7 @@ from pyrogram import Client
 from config import API_ID, API_HASH, BOT_TOKEN
 
 app = client(
-    "copy_right_bot",
+    "client",
     api_id=API_ID,
     api_hash=API_HASH,
     bot_token=BOT_TOKEN
