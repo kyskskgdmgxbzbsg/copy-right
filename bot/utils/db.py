955bc5be519f1c9c111d0c6a8d373eda94b6c372
kyskskgdmgxbzbsg <linuxofficial41@gmail.com>
2025-05-24 15:51:09 +0530

    Update db.py

--- bot/utils/db.py
+++ bot/utils/db.py
@@ -27,3 +27,34 @@ def add_user(user_id):
 
 def get_all_users():
     return [u["user_id"] for u in users.find()]
+filter_db = db["filter"]
+
+def add_blacklist_word(chat_id, word):
+    filter_db.update_one(
+        {"chat_id": chat_id},
+        {"$addToSet": {"blacklist": word.lower()}},
+        upsert=True
+    )
+
+def add_whitelist_word(chat_id, word):
+    filter_db.update_one(
+        {"chat_id": chat_id},
+        {"$addToSet": {"whitelist": word.lower()}},
+        upsert=True
+    )
+
+def get_blacklist_words(chat_id):
+    data = filter_db.find_one({"chat_id": chat_id})
+    return data.get("blacklist", []) if data else []
+
+def get_whitelist_words(chat_id):
+    data = filter_db.find_one({"chat_id": chat_id})
+    return data.get("whitelist", []) if data else []
+
+def is_abusive(word, chat_id):
+    word = word.lower()
+    whitelist = get_whitelist_words(chat_id)
+    if word in whitelist:
+        return False
+    blacklist = get_blacklist_words(chat_id)
+    return word in blacklist
