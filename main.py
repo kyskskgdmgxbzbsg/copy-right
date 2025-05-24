32939b41bf66ec0b7d813f454e508e5ec56bd37b
kyskskgdmgxbzbsg <linuxofficial41@gmail.com>
2025-05-24 17:34:22 +0530

    Update main.py

--- main.py
+++ main.py
@@ -10,7 +10,7 @@ from bot.commands import ban
 from bot.commands import mute
 from bot.commands import stats
 from bot.commands import ping
-from bot.core import cilent
+from bot.core.cilent import app
 from pyrogram import cilent
 from config import API_ID, API_HASH, BOT_TOKEN
 import logging
