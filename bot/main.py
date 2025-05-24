57c1fa72bb03a53268b3fdfc8e9aad0b933b6496
kyskskgdmgxbzbsg <linuxofficial41@gmail.com>
2025-05-24 16:54:52 +0530

    Delete bot/main.py

--- bot/main.py
+++
@@ -1,8 +0,0 @@
-from bot.commands import ping
-from pyrogram import Client, filters
-from pyrogram.types import CallbackQuery
-
-@Client.on_callback_query(filters.regex("close"))
-async def close_message(client: Client, callback_query: CallbackQuery):
-    await callback_query.message.delete()
-    await callback_query.answer()
