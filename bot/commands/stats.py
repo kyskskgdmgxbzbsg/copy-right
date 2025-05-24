4bdf412a7ad4a6cfeee4510e996ad337c16e50bc
kyskskgdmgxbzbsg <linuxofficial41@gmail.com>
2025-05-24 15:29:25 +0530

    Create stats.py

---
+++ bot/commands/stats.py
@@ -0,0 +1,14 @@
+import os
+from pyrogram import Client, filters
+from pyrogram.types import Message
+from bot.keyboards.start_buttons import start_buttons
+
+@Client.on_message(filters.command("stats") & filters.private)
+async def stats_command(client: Client, message: Message):
+    caption = "**Bot Stats:**\n\n- Total Users: coming soon\n- Uptime: coming soon\n- Version: v1.0"
+
+    photo_path = "static/stats.png"
+    if os.path.exists(photo_path):
+        await message.reply_photo(photo=photo_path, caption=caption, reply_markup=start_buttons())
+    else:
+        await message.reply(caption, reply_markup=start_buttons())
