64808615b3c7f405f2ee54b2e2504116103f39d2
kyskskgdmgxbzbsg <linuxofficial41@gmail.com>
2025-05-24 18:06:25 +0530

    Update main.py

--- main.py
+++ main.py
@@ -10,6 +10,7 @@ from bot.commands import ban
 from bot.commands import mute
 from bot.commands import stats
 from bot.commands import ping
+from bot.keyboards.start_buttons import start_buttons
 from bot.core import client
 from pyrogram import client
 from config import API_ID, API_HASH, BOT_TOKEN
