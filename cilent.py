97a5e40422bf3b51e12d5e3db7bfe92ba202639b
kyskskgdmgxbzbsg <linuxofficial41@gmail.com>
2025-05-24 17:23:34 +0530

    Update cilent.py

--- cilent.py
+++ cilent.py
@@ -1,5 +1,1 @@
-# bot/core/client.py
-from pyrogram import client
-from config import API_ID, API_HASH, BOT_TOKEN
-
-app = Client("copy_right_bot", api_id=API_ID, api_hash=API_HASH, bot_token=BOT_TOKEN)
+# You can leave this empty
