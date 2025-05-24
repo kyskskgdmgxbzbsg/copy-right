d4ff21d6b3c98d04b748df4b2d1f8aee5312f7ed
kyskskgdmgxbzbsg <linuxofficial41@gmail.com>
2025-05-24 16:49:54 +0530

    Update config.py

--- config.py
+++ config.py
@@ -7,7 +7,7 @@ load_dotenv()
 API_ID = os.getenv("API_ID", "21546320")
 API_HASH = os.getenv("API_HASH", "c16805d6f2393d35e7c49527daa317c7")
 BOT_TOKEN = os.getenv("BOT_TOKEN", "8020578503:AAFWeiecAUXOmzoOIzzTvnZ8BdcluskMSVk")
-MONGO_DB_URI = os.getenv("MONGO_DB_URI", "mongodb+srv://manoranjanhor43:somuxd@manoranjan.wsglmdq.mongodb.net/?retryWrites=true&w=majority&appName=Manoranjan")
+MONGO_URI = os.getenv("MONGO_DB_URI", "mongodb+srv://manoranjanhor43:somuxd@manoranjan.wsglmdq.mongodb.net/?retryWrites=true&w=majority&appName=Manoranjan")
 OWNER_ID = os.getenv("OWNER_ID", "6908972904")
 LOG_GROUP_ID = os.getenv("LOG_GROUP_ID", "-1002100433415")
 
