860e8ea6b548daa28c69465c2e449a4971b4351b
kyskskgdmgxbzbsg <linuxofficial41@gmail.com>
2025-05-24 15:26:16 +0530

    Create config.py

---
+++ config.py
@@ -0,0 +1,7 @@
+import os
+
+API_ID = int(os.getenv("API_ID", "123456"))
+API_HASH = os.getenv("API_HASH", "your_api_hash")
+BOT_TOKEN = os.getenv("BOT_TOKEN", "your_bot_token")
+OWNER_ID = int(os.getenv("OWNER_ID", "123456789"))
+BOT_USERNAME = os.getenv("BOT_USERNAME", "YourBotUsername")
